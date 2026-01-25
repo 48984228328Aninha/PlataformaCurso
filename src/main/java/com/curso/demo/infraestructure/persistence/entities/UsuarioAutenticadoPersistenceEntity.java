05851e0dc78829625f37ab5fd78a38644a50da2f
48984228328Aninha <Amanda_rodrigues.de.siqueira@hotmail.com>
2026-01-25 16:25:04 -0300

    chore: add methods in UsuarioAutenticadoPersistenceEntity

--- src/main/java/com/curso/demo/infraestructure/persistence/entities/UsuarioAutenticadoPersistenceEntity.java
+++ src/main/java/com/curso/demo/infraestructure/persistence/entities/UsuarioAutenticadoPersistenceEntity.java
@@ -3,22 +3,49 @@ package com.curso.demo.infraestructure.persistence.entities;
 import java.util.Collection;
 
 import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.security.core.GrantedAuthority;
-import java.util.Collections;
+import java.util.List;
 
 public class UsuarioAutenticadoPersistenceEntity implements UserDetails {
+    private final UsuarioPersistenceEntity usuario;
+
+    public UsuarioAutenticadoPersistenceEntity(UsuarioPersistenceEntity usuario) {
+        this.usuario = usuario;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.emptyList();
+        return List.of(() -> "read");
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return usuario.getSenhaUsuario();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return usuario.getUsuarioNome();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
